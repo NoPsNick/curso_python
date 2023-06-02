48bcb39f25ec3d4c6e0ebc148baa2d6eba2f3441
Francisco Serafim <64261693+NoPsNick@users.noreply.github.com>
2023-06-02 11:53:59 -0300

    Finalizando o APP3
    Foi feito o aplicativo 3, onde você pergunta as dimensões da tela, a cor da tela(branco ou preto apenas), o que você deseja desenhar(quadrado ou retângulo apenas), as dimensões do quadrado ou do retângulo, juntamente de suas cores, com um loop de quantos objetos você deseja desenhar e por fim salvando em um arquivo canvas.png.

--- App-3-Project-Math-Painting/main.py
+++ App-3-Project-Math-Painting/main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
+from canvas import Canvas
+from shapes import Rectangle, Square
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Perguntando a largura e a altura que enquadrará o(s) desenho(s)
+canvas_width = int(input("Coloque a largura do canvas: "))
+canvas_height = int(input("Coloque a altura do canvas: "))
+
+colors = {"branco": (255, 255, 255), "preto": (0, 0, 0)}
+canvas_color = input('Diga a cor do canvas, "branco" ou "preto" apenas: ')
+
+canvas = Canvas(height=canvas_height, width=canvas_width, color=colors[canvas_color])
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+while True:
+    shape_type = input("Gostaria de desenhar um 'quadrado' ou um 'retangulo'(digite sair para parar): ")
+    # Caso tenha digitado retângulo
+    if shape_type.lower() == 'retangulo':
+        rec_x = int(input("x do retângulo: "))
+        rec_y = int(input("y do retângulo: "))
+        rec_width = int(input("largura do retângulo: "))
+        rec_height = int(input("altura do retângulo: "))
+        red = int(input("O quão vermelho quer seu retângulo: "))
+        green = int(input("O quão verde quer seu retângulo: "))
+        blue = int(input("O quão azul quer seu retângulo: "))
 
+        # Criando um retângulo
+        r1 = Rectangle(x=rec_x, y=rec_y, height=rec_height, width=rec_width, color=(red, green, blue))
+        r1.draw(canvas)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Caso tenha digitado quadrado
+    if shape_type.lower() == 'quadrado':
+        squ_x = int(input("x do quadrado: "))
+        squ_y = int(input("y do quadrado: "))
+        squ_side = int(input("tamanho do quadrado: "))
+        red = int(input("O quão vermelho quer seu quadrado: "))
+        green = int(input("O quão verde quer seu quadrado: "))
+        blue = int(input("O quão azul quer seu quadrado: "))
+        color = (red, green, blue)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        # Criando um quadrado
+        s1 = Square(x=squ_x, y=squ_y, side=squ_side, color=(red, green, blue))
+        s1.draw(canvas)
+
+    if shape_type.lower() == "sair":
+        break
+
+    else:
+        print("Você digitou algo errado, apenas é possível escrever: quadrado | retangulo | sair")
+
+canvas.make("canvas.png")
