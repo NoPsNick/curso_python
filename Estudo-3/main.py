9ad7afc1affe1ba53c526e53b0b3e6011b505b6d
Francisco Serafim <64261693+NoPsNick@users.noreply.github.com>
2023-06-20 10:18:45 -0300

    Estudo 3, começo
    Foi feito um pequeno esqueleto de usuário e mensagem.

---
+++ Estudo-3/main.py
@@ -0,0 +1,55 @@
+from random import randint
+
+
+class Pessoa:
+
+    def __init__(self, id, email, nome, usuario, senha):
+        self.id = id
+        self.email = email
+        self.nome = nome
+        self.usuario = usuario
+        self.senha = senha
+
+    def enviar_msg(self, ident, titulo, mensagem, destinatario):
+        remetente = self
+
+        return Mensagem(ident, titulo, mensagem, remetente, destinatario)
+
+    def pegar_msg(self, msgs):
+        lista = []
+        for msg in msgs:
+            if msg.destinatario == self:
+                lista.append(msg)
+        return lista
+
+
+class Mensagem:
+
+    def __init__(self, id, titulo, mensagem, remetente, destinatario):
+        self.id = id
+        self.titulo = titulo
+        self.mensagem = mensagem
+        self.remetente = remetente
+        self.destinatario = destinatario
+
+    def formatar(self):
+        identificador = self.id
+        titulo = self.titulo
+        mensagem = self.mensagem
+        remetente = self.remetente
+        return f"[{identificador}] {titulo}:\n" \
+               f"{mensagem}\n" \
+               f"Enviado por: {remetente.usuario}."
+
+
+pessoa1 = Pessoa(randint(1, 100), "email1", "francisco", "xico", "abc123")
+pessoa2 = Pessoa(randint(1, 100), "email2", "felipe", "frufet", "123abc")
+
+mensagem_da_pessoa1 = pessoa1.enviar_msg(randint(1, 100), "Titulo1", "mensagem1", pessoa2)
+mensagem_da_pessoa2 = pessoa2.enviar_msg(randint(1, 100), "Titulo2", "mensagem2", pessoa1)
+mensagem_da_pessoa1_2 = pessoa1.enviar_msg(randint(1, 100), "Titulo3", "mensagem3", pessoa2)
+mensagem_da_pessoa2_2 = pessoa2.enviar_msg(randint(1, 100), "Titulo4", "mensagem4", pessoa1)
+
+pegar = pessoa2.pegar_msg([mensagem_da_pessoa1, mensagem_da_pessoa2, mensagem_da_pessoa1_2, mensagem_da_pessoa2_2])
+for msg in pegar:
+    print(msg.formatar())
